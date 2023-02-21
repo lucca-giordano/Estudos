e14f18dab6836ae81538e20a8feac10ea5eb3464
Lucca Giordano <giordanolucca871@gmail.com>
2023-02-21 02:12:59 -0300

    Update for-loop.cpp

--- C++/Estudos por conta/for-loop.cpp
+++ C++/Estudos por conta/for-loop.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+/* O operador for é usado para executar um bloco de código um número determinado de vezes.
+    O for é composto por três partes:
+        1. Inicialização: é executada uma vez antes do início do loop
+        2. Condição: é verificada no início de cada iteração do loop. Se for verdadeira, o loop continua. Se for falsa, o loop termina.
+        3. Incremento: é executado no final de cada iteração do loop, antes da próxima verificação da condição.
+*/
+
 int main(){
 
     for(int i = 3; i > 0; i--){
