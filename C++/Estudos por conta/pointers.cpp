dba71abf64dbc4e6eaceae7a742d324203f1ad2a
Lucca Giordano <giordanolucca871@gmail.com>
2023-02-23 19:36:13 -0300

    Update pointers.cpp

--- C++/Estudos por conta/pointers.cpp
+++ C++/Estudos por conta/pointers.cpp
@@ -3,12 +3,14 @@
 using namespace std;
 
 int main(){
-    string name = "Joao";
+    // This code creates a variable called name and assigns the value "Joao" to it
+    string name = "Joao"; 
+
+    // This code creates a pointer called pName and assigns the address of the variable name to it
     string *pName = &name;
 
     cout << "Name: " << name << endl;
     cout << "Name address: " << &name << endl;
-
     cout << "Pointer name: " << pName << endl;
     cout << "Pointer name value: " << *pName << endl;
 
