eee95830ddc1207e929911dfb8b285be9b0bfad9
Lucca Giordano <giordanolucca871@gmail.com>
2023-02-24 02:34:42 -0300

    Update pointers.cpp

--- C++/Estudos por conta/pointers.cpp
+++ C++/Estudos por conta/pointers.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
 
     string carros[3] = {"Ferrari", "Lamborghini", "Porsche"};
-    string *pCarros = &carros;
+    string *pCarros = carros;
 
     cout << "Carros: " << carros << endl;
     cout << "pCarros: " << pCarros << endl;
