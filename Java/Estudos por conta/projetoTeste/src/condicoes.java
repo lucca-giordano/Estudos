bd394006d3f876234be117c9e05966f2e7ed3319
Lucca Giordano <giordanolucca871@gmail.com>
2023-02-27 22:22:56 -0300

    Condicionais

---
+++ Java/Estudos por conta/projetoTeste/src/condicoes.java
@@ -0,0 +1,21 @@
+/*
+ * Assim como em toda linguagem de programação, o Java também possui estruturas de controle de fluxo,
+ * que são utilizadas para controlar o fluxo de execução do programa.
+ * 
+ * Estruturas de controle de fluxo são conhecidas como condicionais, seu funcionamento gira em torno de 
+ * uma condição, que pode ser verdadeira ou falsa, e dependendo do resultado dessa condição, o programa
+ * irá executar uma ação ou outra.
+ * 
+ * No Java, temos várias estruturas de controle de fluxo, porém vamos começar com as mais básicas:
+ * 
+ * if: executa uma ação se a condição for verdadeira
+ * else: executa uma ação se a condição for falsa
+ * else if: executa uma ação se a condição for verdadeira, e outra ação se a condição for falsa
+ * 
+ */
+
+public class condicoes {
+    public static void main(String[] args) {
+        
+    }
+}
