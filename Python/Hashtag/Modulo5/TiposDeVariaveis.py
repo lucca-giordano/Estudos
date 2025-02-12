51d2e1af45ddb1697abc2be14313b7539d434458
Lucca Giordano <luccagiordano05@gmail.com>
2025-02-12 14:16:48 -0300

    estudos hashtag

---
+++ Python/Hashtag/Modulo5/TiposDeVariaveis.py
@@ -0,0 +1,19 @@
+faturamento = 1000
+print(type(faturamento))
+
+produto = 'Notebook'
+print(type(produto))
+
+preco = 2500.00
+print(type(preco))
+
+disponivel = True
+print(type(disponivel))
+
+print(f"O faturamento é de {faturamento} reais")
+
+# A função type() retorna o tipo da variável
+# int = inteiro
+# str = string
+# float = ponto flutuante
+# bool = booleano
